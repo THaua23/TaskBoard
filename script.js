19bf4be81be2b72a95bdda719ca50b7e7a56c280
Thauã <thaua23sl@gmail.com>
2025-11-20 19:40:07 -0300

    fix bugs of trash can and card drop position

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const board = document.getElementById('board')// quadro principal
 
 let dragCard // recebe o card arrastado
 let mouseX, mouseY
+let cardWidth, cardHeight
 
 let zIndex = 0
 
@@ -33,7 +34,17 @@ board.addEventListener('dblclick', (event) => {
             dragCard.classList.add('drag-card')
             event.dataTransfer.effectAllowed = 'move'
 
+            cardWidth = dragCard.offsetWidth
+            cardHeight = dragCard.offsetHeight
+
             trash.classList.add('show-trash-can')
+        })
+
+        // remove o erro de transparência do card
+        card.addEventListener('dragend', () => {
+            dragCard.classList.remove('drag-card')
+            trash.classList.remove('show-trash-can')
+            trash.classList.remove('animate-trash')
         })
 
         //adciona o card no quadro
@@ -53,33 +64,37 @@ board.addEventListener('dragover', (event) => {
     event.preventDefault()
 })
 
-// soltar o card
+// soltar o card no quadro
 board.addEventListener('drop', (event) => {
     event.preventDefault() // sempre necessário
     if (dragCard) {
         zIndex++
         // pega a posição do mouse no momento do drop
-        const x = event.clientX - 50
-        const y = event.clientY - 50
-        dragCard.style.top = y + 'px'
-        dragCard.style.left = x + 'px'
-        dragCard.classList.remove('drag-card')
+        const x = event.clientX
+        const y = event.clientY
+        dragCard.style.top = (y - cardHeight/2) + 'px'
+        dragCard.style.left = (x - cardWidth/2) + 'px'
         dragCard.style.zIndex = zIndex
-
-        trash.classList.remove('show-trash-can')
     }
 })
 
 // altera a cor do card
-function changeColor(color) {
-    cardColor = color
-}
+function changeColor(color) {cardColor = color}
+
 
 // apagar card ao dropá-lo na lixeira
 trash.addEventListener('drop', () => {
     let soundTrash = document.getElementById('sound-trash')
-    soundTrash.volume = 0.05
+    soundTrash.volume = 0.1
     soundTrash.playbackRate = 1.5 //velocidade da música
     soundTrash.play()
     dragCard.remove()
+})
+
+// animação da lixeira
+trash.addEventListener('dragover', () => {
+    trash.classList.add('animate-trash')
+})
+trash.addEventListener('dragleave', () => {
+    trash.classList.remove('animate-trash')
 })
